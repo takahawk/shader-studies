3ba8848a3e217ecf0b868b57249eeceaa1818140
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 02:37:38 +0200

    Solve Shader school matrices problem

--- shader-school/05-intro-5/mandelbrot.glsl
+++ shader-school/05-intro-5/mandelbrot.glsl
@@ -1,7 +1,19 @@
-bool mandelbrot(highp vec2 c) {
+vec2 calculateMandelbrot(vec2 z, vec2 c) {
+  return vec2(z.x*z.x - z.y*z.y, 2.0*z.x * z.y) + c;
+}
 
-  //Test if the point c is inside the mandelbrot set after 100 iterations
-  vec2 z = vec2(0.0);
+bool mandelbrotConverges(vec2 z) {
+  return length(z) < 2.;
+}
+
+bool mandelbrot(highp vec2 c) {
+  vec2 z = vec2(0.,0.);
+
+  for (int i = 0; i < 101; i++) {
+    z = calculateMandelbrot(z, c); 
+  }
+  if (mandelbrotConverges(z))
+      return true;
 
   return false;
 }
