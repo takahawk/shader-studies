6e871d17db9ef03e5d5b2df00f2f89561adcc971
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 20:36:54 +0200

    Solve Shader school reflection problem (ooh, that was cool)

--- shader-school/15-geom-4/reflect.glsl
+++ shader-school/15-geom-4/reflect.glsl
@@ -1,11 +1,13 @@
 highp mat4 reflection(highp vec3 n) {
 
-  //TODO: Return a matrix that reflects all points about the plane passing through the origin with normal n
-
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  // p - 2.0 * dot(n, p) * n / dot(n, n) =
+  // p - 2.0 * (n.x * p.x + n.y * p.y + n.z * p.z) * n / (n.x * n.x + n.y * n.y + n.z * n.z) =
+  // p.x = p.x - 2.0 * (n.x * p.x + n.y * p.y + n.z * p.z) * n.x / (n.x * n.x + n.y * n.y + n.z * n.z)
+  float nn = dot(n, n);
+  return mat4(nn - 2.*n.x*n.x, -2.*n.x*n.y,     -2.*n.x*n.z, 0,
+              -2.*n.y*n.x,     nn - 2.*n.y*n.y, -2.*n.y*n.z, 0, 
+              -2.*n.z*n.x,     -2.*n.z*n.y,     nn - 2.*n.z*n.z, 0,
+              0, 0, 0, nn);
 }
 
 #pragma glslify: export(reflection)
