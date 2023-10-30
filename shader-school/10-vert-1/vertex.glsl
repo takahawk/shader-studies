517abce819a8e8977b335f68598a15a0f6b7b276
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 03:25:16 +0200

    Solve Shader school vertex shaders first problem

--- shader-school/10-vert-1/vertex.glsl
+++ shader-school/10-vert-1/vertex.glsl
@@ -5,8 +5,7 @@ uniform float theta;
 attribute vec2 position;
 
 void main() {
-
-  //TODO: rotate position by theta radians about the origin
-
-  gl_Position = vec4(position, 0, 1.0);
+  float x = position.x * cos(theta) - position.y * sin(theta);
+  float y = position.x * sin(theta) + position.y * cos(theta);
+  gl_Position = vec4(x, y, 0, 1.0);
 }
