0c3c40390f85c3fee34e96baa6bddbfb22160ec4
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 19:54:04 +0200

    Solve Shader school clip translate problem

--- shader-school/12-geom-1/transforms.glsl
+++ shader-school/12-geom-1/transforms.glsl
@@ -5,8 +5,6 @@ attribute vec3 position;
 uniform mat4 model, view, projection;
 
 void main() {
-
-  //TODO: Apply the model-view-projection matrix to `position`
   vec4 vertex = vec4(position, 1.0);
   gl_Position = projection * view * model * vertex;
 }
