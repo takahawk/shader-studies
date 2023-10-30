48753471f9ff0455596d10262d626129156b05b9
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 02:55:10 +0200

    Solve Shader school fragments first problem

--- shader-school/07-frag-1/fragment.glsl
+++ shader-school/07-frag-1/fragment.glsl
@@ -4,10 +4,7 @@ precision highp float;
 #define OUTSIDE_COLOR   vec4(0.3804, 0.7647, 1.0, 1.0)
 
 void main() {
-
-  //TODO: Replace this with a function that draws a circle at (256.5,256.5) with radius 128
-  
-  if(gl_FragCoord.y > 256.0) {
+  if(length(gl_FragCoord.xy - vec2(256.5,256.5)) < 128.) {
     gl_FragColor = CIRCLE_COLOR;
   } else {
     gl_FragColor = OUTSIDE_COLOR;
