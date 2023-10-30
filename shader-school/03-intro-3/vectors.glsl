c88ab4dbb8992cfdc13b7a28e31d6ca42b381d45
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-10-30 02:24:44 +0200

    Solve Shaders school vectors problem

--- shader-school/03-intro-3/vectors.glsl
+++ shader-school/03-intro-3/vectors.glsl
@@ -1,8 +1,5 @@
 highp vec2 func(highp vec2 a, highp vec2 b) {
-
-  //TODO: Implement the exercise here
-
-  return vec2(1, 0);
+  return normalize(length(b) * a + length(a) * b);
 }
 
 //Do not change this line
