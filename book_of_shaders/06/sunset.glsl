fa2753085a66b6320d6f694b084511efaa6d2827
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2023-11-08 14:30:32 +0200

    Add sunset/sunrise animation

--- book_of_shaders/06/sunset.glsl
+++ book_of_shaders/06/sunset.glsl
@@ -9,6 +9,7 @@ vec3 indigo = vec3(0.396,0.384,0.561);
 vec3 darkRed = vec3(0.733,0.18,0.063);
 vec3 orange = vec3(0.98,0.733,0.361);
 vec3 beige = vec3(0.969,0.894,0.729);
+vec3 yellow = vec3(1., 1., 0.);
 
 vec3 get_pct(float y) {
     return vec3(pow(y, 2.) / 0.25, y, y);
@@ -24,8 +25,9 @@ void main() {
     vec3 pct = vec3(pow(uv.y, 2.) / 0.25, uv.y, uv.y); // smoothstep(0., 1., uv.y));
 
     if (uv.y > 0.) {
+        vec3 col = mix(yellow, darkRed, smoothstep(-1., 1., sin(u_time)));
         if (uv.y < 0.2)
-        	color = mix(darkRed, orange, get_pct(uv.y) / 0.2);
+        	color = mix(col, orange, get_pct(uv.y) / 0.2);
         else if (uv.y < 0.5)
         	color = mix(orange, indigo, get_pct(uv.y - 0.2) / 0.3);
         else
